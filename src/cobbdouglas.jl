62c1cceb5e6f5a0054077af869e04613bf8f7a94
theplatters <fscharnreitner@gmail.com>
2024-06-07 14:27:31 +0200

    fixed mistakes

--- src/cobbdouglas.jl
+++ src/cobbdouglas.jl
@@ -1,6 +1,6 @@
 struct CBELasticities <: Elasticities
-	α::Float64
-	β::Float64
+	α::Vector{Float64}
+	β::Vector{Float64}
 end
 
 
@@ -19,56 +19,55 @@ end
 
 function generalized_problem(x, data::Data, price!, intermediary_demand, consumption)
 	N = length(data.λ)
-	p = @view x[1:N]
-	y = @view x[N+1:end]
+	p = max.(0,x[1:N])
+	y = max.(0,x[N+1:end])
 
 
 	out = zeros(eltype(x), 2 * N)
 
-	price!(view(out,1:N), p, y, data)
+	price!(view(out, 1:N), p, y, data)
 	out[N+1:end] .= y - intermediary_demand(p, y, data) - consumption(p, y, data)
 	out
 end
 
 function wages(p, y, data)
-	(;α, β) = data.elasticities
-	(;supply_shock, demand_shock) = data.shocks
-	α * p .* y .* data.labor_share .^ -1
+	(; α, β) = (data.elasticities)
+	(; supply_shock, demand_shock) = data.shocks
+	α .* p .* y .* data.labor_share .^ -1
 end
 
 function cobb_douglas_intermediary_demand(p, y, data)
-	(;α, β) = data.elasticities
-	(;supply_shock, demand_shock) = data.shocks
+	(; α, β) = (data.elasticities)
+	(; supply_shock, demand_shock) = data.shocks
 
 	w = wages(p, y, data)
-	r = data.Ω * p
+	r = p .^ data.Ω
 
-
-	(β / (α + β)) .* cobb_douglas_costfun(p, y, data) .* r .^ -1 .* (data.Ω * y)
+	(data.Ω') * (β .* y .* cobb_douglas_costfun(p, y, data)) .* inv.(p)
 end
 
 
 function cobb_douglas_costfun(p, y, data)
-	(;supply_shock, demand_shock) = data.shocks
-	(;α, β) = data.elasticities
-	w = wages(p, y, data) 
-	r =  (data.Ω * p)
-	(α + β) * supply_shock .^ -inv(α + β) .* y .^ inv(α + β)  * (α^(-α/(α + β)) * β^(-β/(α + β))) .* w .^ (α / (α + β)) .* r .^ (β / (α + β))
+	(; α, β) = (data.elasticities)
+	(; supply_shock, demand_shock) = data.shocks
+	w = wages(p, y, data)
+	r = p .^ data.Ω
+	inv.(supply_shock) .* (w .^ α) .* (prod(r, dims = 2) .^ β)
 end
 
 function cobb_douglas_prices!(out, p, y, data::CBData)
-	out .= p .- cobb_douglas_costfun(p, y, data)
+	out .= (p .- cobb_douglas_costfun(p, y, data))
 	nothing
 end
 
 
 function cobb_douglas_consumption(p, y, data)
-	(;α, β) = data.elasticities
-	(;supply_shock, demand_shock) = data.shocks
+	(; α, β) = data.elasticities
+	(; supply_shock, demand_shock) = data.shocks
 	w = wages(p, y, data)
-	r = data.Ω * p
+	r = p .^ data.Ω
 	C = w' * data.labor_share
-	C * demand_shock .* p .^ (-0.95) .* data.consumption_share
+	C * demand_shock .* p .^ (-0.95) .* data.consumption_share #taking a CES consumption function for now
 end
 
 function read_data_cb(filename::String)
@@ -80,20 +79,30 @@ function read_data_cb(filename::String)
 
 	Ω, consumption_share, factor_share, λ, labor_share, consumption_share_go, grossy = generateData(io)
 	#return a mutable structure element (see above):
-	return CBData(io, Ω, consumption_share, factor_share, λ, labor_share, consumption_share_go, Shocks(ones(71), ones(71)), CBELasticities(0.3,0.7), grossy)
+	return CBData(io, Ω, consumption_share, factor_share, λ, labor_share, consumption_share_go, Shocks(ones(71), ones(71)), CBELasticities(factor_share, 1 .- factor_share), grossy)
 end
 
 function solve_cobb_douglas_modell(
 	data::Data,
 	shocks::Shocks,
-    elasticities::Elasticities,
-	init = Complex.(vcat(ones(length(data.λ)), data.λ)))
+	elasticities::Elasticities,
+	init = (vcat(ones(length(data.λ)), data.λ)))
 
-    set_shocks!(data, shocks)
-    set_elasticities!(data, elasticities)
+	set_shocks!(data, shocks)
+	set_elasticities!(data, elasticities)
 
 	f = NonlinearSolve.NonlinearFunction((x, u) -> generalized_problem(x, u, cobb_douglas_prices!, cobb_douglas_intermediary_demand, cobb_douglas_consumption))
 	prob = NonlinearSolve.NonlinearProblem(f, init, data)
 
-	sol = NonlinearSolve.solve(prob)
+	x = NonlinearSolve.solve(prob)
+	p = x[1:length(data.consumption_share)]
+	q = x[(length(data.consumption_share)+1):end]
+	df = DataFrames.DataFrame(
+		Dict("prices" => p,
+			"quantities" => q,
+			"sectors" => data.io.Sektoren[1:71],
+			"value_added" => data.labor_share .* wages(p, q, data)),
+	)
+
+	df
 end

4a097ab3803be7f063a09afd9d4003f0536bb595
theplatters <fscharnreitner@gmail.com>
2024-12-12 13:26:28 +0100

    plots plots plots

--- tests/compare_models.jl
+++ tests/compare_models.jl
@@ -76,7 +76,7 @@ end
 
 function gradient(shocks, labor_slack, labor_reallocation, elasticity, sol, el, nominal = false)
 	s = copy(sol)
-	len = 1000
+	len = 500
 	gdp = ones(len)
 	mean_prices = ones(len)
 	arr = copy(el)
@@ -98,7 +98,6 @@ function gradient(shocks, labor_slack, labor_reallocation, elasticity, sol, el, 
 			gdp[idx] = NaN
 			mean_prices[idx] = NaN
 		end
-		@show idx, arr, gdp[idx] mean_prices[idx]
 	end
 	return (gdp, mean_prices)
 end
@@ -136,10 +135,11 @@ function plot_elasticities(results; title = "Real GDP", cd = sol_cd, ylims = (97
 	ga = f[1, 1] = GridLayout()
 	ax = [Axis(ga[1, 1], ylabel = "GDP", ytickformat = "{:.2f}%", title = "0.9"),
 		Axis(ga[1, 2], xlabel = "Elasticity", ytickformat = "{:.2f}%", title = "0.5"),
-		Axis(ga[1, 3], ytickformat = "{:.2f}%", title = "0.1")]
+		Axis(ga[2, 1], ytickformat = "{:.2f}%", title = "0.1"),
+		Axis(ga[2, 2], ytickformat = "{:.2f}%", title = "0.1")]
 
 	supertitle = Label(f[0, :], title, fontsize = 40, tellwidth = false)
-	linkaxes!(ax[1], ax[2], ax[3])
+	linkaxes!(ax[1], ax[2], ax[3], ax[4])
 	for (i, el) in enumerate(results)
 		lines!(ax[i], 0.015 .. 0.9, 100 .* reverse(el.ϵ), label = "Elasticity between goods")
 		lines!(ax[i], 0.015 .. 0.9, 100 .* reverse(el.θ), label = "Elasticity between labour and goods")
@@ -163,7 +163,7 @@ function plot_prices(results; title = "Real GDP", cd = sol_cd, ylims = (97, 103)
 		Axis(ga[2, 1], ytickformat = "{:.2f}%", title = "0.2"),
 		Axis(ga[2, 2], ytickformat = "{:.2f}%", title = "0.05")]
 
-	supertitle = Label(f[0, :], title, fontsize = 40, tellwidth = false)
+	supertitle = Label(f[0, :], title, fontsize = 20, tellwidth = false)
 	linkaxes!(ax[1], ax[2], ax[3], ax[4])
 	for (i, el) in enumerate(results)
 		lines!(ax[i], 0.015 .. 0.9, 100 .* reverse(el.mean_prices_ϵ), label = "Elasticity between goods")
@@ -177,9 +177,12 @@ function plot_prices(results; title = "Real GDP", cd = sol_cd, ylims = (97, 103)
 end
 
 #shock 4 different sectors
-for sector in rand(data.io.Sektoren,4)
+for sector in rand(data.io.Sektoren[1:71],4)
 	shocks = Shocks(ones(71), ones(71))
-	calculate_investment!(shocks, data, [50000], [sector])
+
+	sector_number = findfirst(==(sector), data.io.Sektoren)
+	shocks.demand_shock[sector_number] = 1.4
+
 	a = elasticity_gradient(shocks, full_labor_slack, false, [0.99, 0.99, 0.99])
 	b = elasticity_gradient(shocks, full_labor_slack, false, [0.7, 0.7, 0.7])
 	c = elasticity_gradient(shocks, full_labor_slack, false, [0.2, 0.2, 0.2])
@@ -191,15 +194,16 @@ for sector in rand(data.io.Sektoren,4)
 	h = elasticity_gradient(shocks, model -> data.labor_share, false, [0.1, 0.1, 0.1])
 
 	
-	p1 = plot_prices([a, b, c, d], cd = sol_cd_ls, title = "Effect of different elasticities on GDP, with labour slack" * sector)
-	p2 = plot_prices([e, f, g, h], cd = sol_cd_ls, title = "Effect of different elasticities on GDP, with labour slack" * sector)
-	show(p1)
-	show(p2)
+	p1 = plot_prices([a, b, c, d], cd = sol_cd_ls, title = "Effect of different elasticities on mean prices, with labour slack" * sector)
+	p2 = plot_prices([e, f, g, h], cd = sol_cd_ls, title = "Effect of different elasticities on mean prices, without labour slack" * sector)
+	p3 = plot_elasticities([a, b, c, d], cd = sol_cd_ls, title = "Effect of different elasticities on GDP with labour slack" * sector)
+	p4 = plot_elasticities([e, f, g, h], cd = sol_cd_ls, title = "Effect of different elasticities on GDP, without labour slack" * sector)
+
+ 	save("plots/elastictiy_gradient_ls_prices_$sector.png" , p1)
+	save("plots/elastictiy_gradient_no_ls_prices_$sector.png", p2)
+	save("plots/elastictiy_gradient_no_ls_$sector.png", p3)
+ 	save("plots/elastictiy_gradient_ls_$sector.png" , p4)
 end
-#= 	save("plots/elastictiy_gradient_ls.png", p1)
-	save("plots/elastictiy_gradient_no_ls.png", p2)
-	save("plots/elastictiy_gradient_lr.png", p3)
- =#
 #=============================================================================
 Simulating labour slack effect
 ===============================================================================#
